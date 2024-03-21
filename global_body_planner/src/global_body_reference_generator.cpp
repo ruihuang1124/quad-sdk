89da6eccb6463743d090b33ddd51cad666306c1f
ruihuang1124 <ruihuang.scut@gmail.com>
2024-03-21 16:35:05 +0800

    add global body reference generator node

---
+++ global_body_planner/src/global_body_reference_generator.cpp
@@ -0,0 +1,4 @@
+//
+// Created by ray on 3/21/24.
+//
+#include "global_body_planner/global_body_reference_generator.h"
---
+++ global_body_planner/src/global_body_reference_generator_node.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ray on 3/21/24.
+//
+
+#include <ros/ros.h>
+
+#include "global_body_planner/global_body_reference_generator.h"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "global_body_reference_generator");
+    ros::NodeHandle nh;
+
+    return 0;
+}
